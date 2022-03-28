e7789faeb34dfc282130c8cc98f29b45373847be
shunyue1320 <tsunyue1320@gmail.com>
2022-03-28 22:09:51 +0800

    chore: h

---
+++ packages/runtime-core/src/h.ts
@@ -0,0 +1,31 @@
+import { isArray, isObject } from '@vue/shared'
+import { createVnode, isVnode } from './vnode'
+
+// 各种用法
+// h('div',{style:{"color"：“red”}},'hello')
+// h('div','hello')
+// h('div',null,'hello','world')
+// h('div',null,h('span'))
+// h('div',null,[h('span')])
+
+export function h(type, propsChildren, children) {
+  const l = arguments.length
+
+  if (l === 2) {
+    if (isObject(propsChildren) && !isArray(propsChildren)) {
+      if (isVnode(propsChildren)) {
+        return createVnode(type, null, [propsChildren])
+      }
+      return createVnode(type, propsChildren)
+    } else {
+      return createVnode(type, null, propsChildren)
+    }
+  } else {
+    if (l > 3) {
+      children = Array.from(arguments).slice(2)
+    } else if (l === 3 && isVnode(children)) {
+      children = [children]
+    }
+    return createVnode(type, propsChildren, children)
+  }
+}
