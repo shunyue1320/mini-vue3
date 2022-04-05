9e2213a8a6ab67dec6359500fe25956e1cc5f048
shunyue1320 <tsunyue1320@gmail.com>
2022-04-05 12:31:26 +0800

    chore: init parse

---
+++ packages/compiler-core/dist/compiler-core.global.js
@@ -0,0 +1,115 @@
+var VueCompilerCore = (() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+  // packages/compiler-core/src/index.ts
+  var src_exports = {};
+  __export(src_exports, {
+    compile: () => compile
+  });
+  function createParserContext(template) {
+    return {
+      line: 1,
+      column: 1,
+      offset: 0,
+      source: template,
+      originalSource: template
+    };
+  }
+  function isEnd(context) {
+    const source = context.source;
+    return !source;
+  }
+  function getCursor(context) {
+    let { line, column, offset } = context;
+    return { line, column, offset };
+  }
+  function advancePositionWithMutation(context, source, endIndex) {
+    let linesCount = 0;
+    let linePos = -1;
+    for (let i = 0; i < endIndex; i++) {
+      if (source.charCodeAt(i) == 10) {
+        linesCount++;
+        linePos = i;
+      }
+    }
+    context.line += linesCount;
+    context.offset += endIndex;
+    context.column = linePos == -1 ? context.column + endIndex : endIndex - linePos;
+  }
+  function advanceBy(context, endIndex) {
+    let source = context.source;
+    advancePositionWithMutation(context, source, endIndex);
+    context.source = source.slice(endIndex);
+  }
+  function parseTextData(context, endIndex) {
+    const rawText = context.source.slice(0, endIndex);
+    advanceBy(context, endIndex);
+    return rawText;
+  }
+  function getSelection(context, start, end) {
+    end = end || getCursor(context);
+    return {
+      start,
+      end,
+      source: context.originalSource.slice(start.offset, end.offset)
+    };
+  }
+  function parseText(context) {
+    let endTokens = ["<", "{{"];
+    let endIndex = context.source.length;
+    for (let i = 0; i < endTokens.length; i++) {
+      let index = context.source.indexOf(endTokens[i], 1);
+      if (index !== -1 && endIndex > index) {
+        endIndex = index;
+      }
+    }
+    const start = getCursor(context);
+    const content = parseTextData(context, endIndex);
+    return {
+      type: 2 /* TEXT */,
+      content,
+      loc: getSelection(context, start)
+    };
+  }
+  function parse(template) {
+    const context = createParserContext(template);
+    const nodes = [];
+    while (!isEnd(context)) {
+      const source = context.source;
+      let node;
+      if (source.startsWith("{{")) {
+        node = "xxx";
+      } else if (source[0] === "<") {
+        node = "qqq";
+      }
+      if (!node) {
+        node = parseText(context);
+      }
+      nodes.push(node);
+      console.log(nodes);
+      break;
+    }
+  }
+  function compile(template) {
+    const ast = parse(template);
+    return ast;
+  }
+  return __toCommonJS(src_exports);
+})();
+//# sourceMappingURL=compiler-core.global.js.map
